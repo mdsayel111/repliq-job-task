4759a7e4a0da2a4aafe0e0b6af62382edf05d308
sayel <mdsayel111@gmail.com>
2025-06-06 07:26:45 +0600

    ok

--- components/shared/pagination/Pagination.jsx
+++ components/shared/pagination/Pagination.jsx
@@ -63,30 +63,3 @@ export default function Pagination() {
     </ul>
   );
 }
-{
-  /* <nav aria-label="Page navigation example">
-  <ul className="inline-flex -space-x-px text-base h-10">
-    <li>
-      <a href="#" className="flex items-center justify-center px-4 h-10 ms-0 leading-tight text-yellow-900 bg-white border border-e-0 border-gray-300 rounded-s-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-white dark:border-gray-700 dark:text-yellow-900 dark:hover:bg-yellow-100 ">Previous</a>
-    </li>
-    <li>
-      <a href="#" className="flex items-center justify-center px-4 h-10 leading-tight text-yellow-900 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-white dark:border-gray-700 dark:text-yellow-900 dark:hover:bg-yellow-100 ">1</a>
-    </li>
-    <li>
-      <a href="#" className="flex items-center justify-center px-4 h-10 leading-tight text-yellow-900 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-white dark:border-gray-700 dark:text-yellow-900 dark:hover:bg-yellow-100 ">2</a>
-    </li>
-    <li>
-      <a href="#" aria-current="page" className="flex items-center justify-center px-4 h-10 text-blue-600 border border-gray-300 bg-blue-50 hover:bg-blue-100 hover:text-blue-700 dark:border-gray-700 dark:bg-gray-700 dark:text-white">3</a>
-    </li>
-    <li>
-      <a href="#" className="flex items-center justify-center px-4 h-10 leading-tight text-yellow-900 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-white dark:border-gray-700 dark:text-yellow-900 dark:hover:bg-yellow-100 ">4</a>
-    </li>
-    <li>
-      <a href="#" className="flex items-center justify-center px-4 h-10 leading-tight text-yellow-900 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-white dark:border-gray-700 dark:text-yellow-900 dark:hover:bg-yellow-100 ">5</a>
-    </li>
-    <li>
-      <a href="#" className="flex items-center justify-center px-4 h-10 leading-tight text-yellow-900 bg-white border border-gray-300 rounded-e-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-white dark:border-gray-700 dark:text-yellow-900 dark:hover:bg-yellow-100 ">Next</a>
-    </li>
-  </ul>
-</nav> */
-}
