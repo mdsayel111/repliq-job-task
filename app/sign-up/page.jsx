8b5bef378f259cce36877b4455a3ad0efa69e3a1
sayel <mdsayel111@gmail.com>
2025-06-07 16:05:29 +0600

    change text

--- app/sign-up/page.jsx
+++ app/sign-up/page.jsx
@@ -66,7 +66,7 @@ export default function SignUp() {
                 type="submit"
                 className="flex w-full justify-center rounded-md bg-yellow-300 px-3 py-1.5 text-sm/6 font-semibold text-yellow-900 shadow-xs hover:bg-yellow-100 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               >
-                Sign in
+                Sign up
               </button>
             </div>
           </form>
